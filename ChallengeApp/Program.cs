d47757f54c3fd727cd1dec1917aec7e5d9800567
AndJ1205 <144378919+AndJ1205@users.noreply.github.com>
2023-09-10 18:58:37 +0200

    Dodaj pliki projektów.

---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ChallengeApp;
+
+Employee employee1 = new Employee("Adam", "Kowal", 25, new int[] { 8, 9, 7, 8, 1 });
+Employee employee2 = new Employee("Anna", "Mucha", 32, new int[] { 6, 6, 9, 8, 7 });
+Employee employee3 = new Employee("Damian", "Dąb", 28, new int[] { 9, 9, 8, 7, 8 });
+
+// Tablica pracowników
+Employee[] employees = { employee1, employee2, employee3 };
+
+// Znajdowanie pracownika z najwyższą sumą ocen
+Employee employeeWithHighestRatings = employees.OrderByDescending(e => e.CalculateTotalRatings()).First();
+
+// Wyświetlanie informacji o pracowniku z najwyższą sumą ocen
+Console.WriteLine("Pracownik z najwyższą sumą ocen:");
+employeeWithHighestRatings.DisplayEmployeeInfo();
+int totalRatings = employeeWithHighestRatings.CalculateTotalRatings();
+Console.WriteLine($"Suma ocen: {totalRatings}");
---
+++ ChallengeApp/User.cs
@@ -0,0 +1,17 @@
+﻿namespace ChallengeApp
+{
+    public class User
+    {
+        private List<int> score = new List<int>();
+        private User(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+    }
+}
